ea1f08bfb215c9244be83657dabdd1ae22c11329
Kejoko <keeganjkochis@gmail.com>
2020-06-29 21:04:17 -0600

    Fix red flickering window

--- juniper/src/App.cpp
+++ juniper/src/App.cpp
@@ -68,7 +68,7 @@ namespace Juniper {
             return -1;
         }
         
-        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        window = glfwCreateWindow(640, 480, title.c_str(), NULL, NULL);
         if (!window) {
             glfwTerminate();
             return -1;
@@ -100,7 +100,7 @@ namespace Juniper {
             
             // Render current stateglClear
             (GL_COLOR_BUFFER_BIT);
-            glfwSwapBuffers(window);
+            //glfwSwapBuffers(window);
             glfwPollEvents();
         }
         
